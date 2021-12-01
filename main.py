0e3e41d4c752a8fb253d3b4daf52721beaddb85e
simplecelery <88178373+simplecelery@users.noreply.github.com>
2021-12-01 23:06:59 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,161 @@
+import threading
+import time
+import bs4
+import requests
+import StellarPlayer
+import re
+import urllib.parse
+from .plugin import Plugin
+
+
+class myplugin(StellarPlayer.IStellarPlayerPlugin, Plugin):
+    def __init__(self,player:StellarPlayer.IStellarPlayer):
+        StellarPlayer.IStellarPlayerPlugin.__init__(self, player)        
+        Plugin.__init__(self)
+        self.detail = None
+        self.play_list = []
+        
+        print(self.data)
+        print(self.categories)
+        
+    @property
+    def movies(self):
+        # to dict
+        return [ dict(zip(['name', 'cover', 'summary', 'category', 'src'], i)) for i in self.paged_data] 
+       
+    def stop(self):
+        return super().stop()
+
+    def start(self):
+        return super().start()
+
+    def makeLayout(self):
+        if self.detail is None:
+            return self.makeLayoutList() 
+        else:
+            return self.makeLayoutDetail() 
+
+    def makeLayoutList(self):
+        category_items = []
+        categories = map(lambda x: f'[{x}]' if x == self.current_category else x, ['全部'] + list(self.categories))
+        for cat in categories:
+            category_items.append({'type':'link','name':cat, 'fontSize':20, '@click':'onCategoryClick', 'width':60})
+
+        grid_layout = {'group':
+                            [
+                                {'type':'image','name':'cover','width':120,'height':150,'@click':'onMovieClick'},
+                                {'type':'label','name':'name','hAlign':'center'},
+                            ],
+                            'dir':'vertical'
+                        }
+        controls = [
+            {'group':category_items, 'name': 'categories', 'height':30},
+            {'type':'space','height':10},
+            {'group':
+                [
+                    {'type':'edit','name':'search_edit','label':'搜索', ':value': 'q'},
+                    {'type':'button','name':'搜电影','@click':'onSearch'}
+                ]
+                ,'height':30
+            },
+            {'type':'space','height':10},
+            {'type':'grid','name':'list','itemlayout':grid_layout,'value':self.movies,'marginSize':5,'itemheight':180,'itemwidth':120},
+            {'group':
+                [
+                    {'type':'space'},
+                    {'group':
+                        [
+                            {'type':'label','name':'cur_page',':value':'cur_page'},
+                            {'type':'link','name':'首页','fontSize':13,'@click':'onClickFirstPage'},
+                            {'type':'link','name':'上一页','fontSize':13,'@click':'onClickFormerPage'},
+                            {'type':'link','name':'下一页','fontSize':13,'@click':'onClickNextPage'},
+                            {'type':'link','name':'末页','fontSize':13,'@click':'onClickLastPage'},
+                            {'type':'label','name':'max_page',':value':'max_page'},
+                        ]
+                        ,'width':0.7
+                    },
+                    {'type':'space'}
+                ]
+                ,'height':30
+            },
+            {'type':'space','height':5}
+        ]
+        return controls
+
+    def makeLayoutDetail(self):
+        detail = self.detail
+        self.play_list = [{'name': name, 'url': url} for name, url in detail['src']]
+        play_list_layout = {'type':'link','name':'name','@click':'onPlayClick'}
+        controls = [
+            {'type':'space','height':10},
+            {'type':'link','name':'返回','@click':'onBackClick', 'fontSize':20, 'height': 20},
+            {'type':'space','height':10},
+            {'group':[
+                    {'type':'image','name':'mediapicture', 'value':detail['cover'],'width':0.5},
+                    {'type':'space','width':10},
+                    {'group':[
+                            {'type':'label','name':'medianame','textColor':'#ff7f00','fontSize':20,'value':detail['name'],'height':40},
+                            {'type':'space','height':10},
+                            {'type':'label','name':'info','textColor':'#005555','value':detail['summary'],'height':200,'vAlign':'top'},
+                            {'type':'space','height':10},
+                            {
+                                'group': {'type':'grid','name':'play_list','itemlayout':play_list_layout,'value':self.play_list,'separator':True,'itemheight':30,'itemwidth':120},
+                            }
+                        ],
+                        'dir':'vertical'
+                    },
+                    {'type':'space','width':10}
+                ],
+                'width':1.0
+            },
+            {'type':'space','height':10}
+        ]
+        return controls
+        
+    def show(self):
+        self.doModal('main',800,750,'影片插件示例',self.makeLayout())
+
+    def onSearch(self,*args):
+        self.updateLayout('main', self.makeLayout())
+
+    def onCategoryClick(self, pageId, control,*args):
+        self.current_category = control.strip('[]')
+        self.updateLayout('main', self.makeLayout())
+
+    def onMovieClick(self, pageId, control, index, *args):
+        self.detail = self.movies[index]
+        self.updateLayout('main', self.makeLayout())
+
+    def onBackClick(self, *args):
+        self.detail = None
+        self.updateLayout('main', self.makeLayout())
+
+    def onPlayClick(self, page, listControl, index, itemControl):
+        item = self.play_list[index]        
+        try:
+            self.player.play(item['url'], caption=f"{self.detail['name']} - {item['name']}")
+        except:
+            self.player.play(item['url'])
+
+    def onClickFirstPage(self, *args):
+        self.page = 1
+        self.updateLayout('main', self.makeLayout())
+
+    def onClickFormerPage(self, *args):
+        if self.page > 1: self.page -= 1
+        self.updateLayout('main', self.makeLayout())
+
+    def onClickNextPage(self, *args):
+        if self.page < self.page_count: self.page += 1
+        self.updateLayout('main', self.makeLayout())
+
+    def onClickLastPage(self, *args):
+        self.page = self.page_count            
+        self.updateLayout('main', self.makeLayout())
+    
+def newPlugin(player:StellarPlayer.IStellarPlayer,*arg):
+    plugin = myplugin(player)
+    return plugin
+
+def destroyPlugin(plugin:StellarPlayer.IStellarPlayerPlugin):
+    plugin.stop()
---
+++ plugin.py
@@ -0,0 +1,74 @@
+import csv
+import os
+import codecs
+import io
+
+PAGE_SIZE = 18
+
+class Plugin:
+    def __init__(self):
+        self.categories = set()
+        data_path = os.path.join(os.path.dirname(__file__), 'data.csv')
+        self.data = list(self.load_data(self.read_file(data_path)))   
+        self.q = ''
+        self.current_category = '全部'
+        self.page = 1         
+
+    def read_file(self, path):
+        size = min(32, os.path.getsize(path))
+        raw = open(path, 'rb').read(size)
+        encoding = 'utf-8-sig' if raw.startswith(codecs.BOM_UTF8) else 'utf-8'
+        with io.open(path, 'r', encoding=encoding) as file:
+            return file.read()
+
+    def load_data(self, str):           
+        f = io.StringIO(str) 
+        reader = csv.reader(f, delimiter=',', quotechar='\"')
+        for row in reader:
+            name, cover, summary, category, src = row
+            if category:
+                self.categories.add(category)
+            sources = []
+            for line in src.split('#'):
+                line = line.strip()
+                if line:
+                    sname = url = ''
+                    items = line.split('$')
+                    if len(items) == 1:
+                        sname, url = '正片', line
+                    else:
+                        sname, url, *_ = items
+                    sources.append((sname, url))
+            yield name, cover, summary, category, sources
+
+    @property
+    def filtered_data(self):
+        qs = self.data
+        q = self.q
+        if q:
+            qs = (i for i in qs if q in i[0]) 
+        if self.current_category != '全部':
+            qs = (i for i in qs if i[3] == self.current_category)
+        return list(qs)
+            
+    @property
+    def paged_data(self):
+        page = self.page - 1
+        return self.filtered_data[page * PAGE_SIZE : page * PAGE_SIZE + PAGE_SIZE]
+
+    @property
+    def page_count(self):
+        return (len(self.filtered_data) + PAGE_SIZE - 1) // PAGE_SIZE
+
+    @property
+    def cur_page(self):
+        return f'第 {self.page} 页'
+
+    @property
+    def max_page(self):
+        return f'共 { self.page_count } 页'
+
+
+if __name__ == '__main__':
+    plugin = Plugin()
+    print(plugin.data)
